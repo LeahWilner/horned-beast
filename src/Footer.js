2ca1fccd98913bacbd9b528876c87492e809b07a
Leah Wilner <leahwilner1@gmail.com>
2023-01-07 18:00:49 -0600

    added main and HB files

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component{
+    render(){
+
+        return(
+
+
+            <h2>Leah Wilner</h2>)
+           }
+        }
+        
+    export default Footer
