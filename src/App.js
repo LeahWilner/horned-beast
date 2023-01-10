94c50ce5a60eae1cb0fb6365925235444ad4f25b
tektechnologies <info@tektechnologies.com>
2023-01-09 19:58:22 -0600

    add edits and updates

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
+
+
 class App extends React.Component {
   render() {
     return (
