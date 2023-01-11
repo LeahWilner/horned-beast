8e12cea4b407a01f0952b6adcf0f4fbef26c452e
Leah Wilner <leahwilner1@gmail.com>
2023-01-10 20:10:17 -0600

    added this.state

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,6 +1,36 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
 
 class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: 0,
+      beastFavorite: false,
+    };
+  }
+  
+  
+  handleLikes = () => {
+    this.setState9({
+      likes: this.state.likes +1,
+    });
+  }
+  
+  
+  beastFavorite = () => {
+    this.setState({
+      beastFavorite: true,
+    })
+  }
+  
+  beastUnfavored = () => {
+    this.setState({
+      beastFavorite: false,
+    })
+  }
+
+
   render() {
     console.log('props?', this.props);
     return(
@@ -8,6 +38,13 @@ class HornedBeast extends React.Component {
       <h2>{this.props.title}</h2>
       <img src={this.props.image_url} alt={this.props.title} title={this.props.title}/>
       <p>{this.props.description}</p>
+
+      <div>{this.state.beastFavorite ? 'My Favorite Beast' : ''}</div>
+      <Button></Button>
+      <Button></Button>
+        
+
+
       </>
     ) 
   }
